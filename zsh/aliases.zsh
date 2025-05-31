0c468e11181adf6fb027b4e34ff4cdd94c0ebdb7
BillCipherVoid <billciphervoid@gmail.com>
2025-06-01 01:25:46 +0300

    Mod zsh/aliases
    add python aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,11 @@
 alias cls=clear
 
-#alias n=nvim
+alias n=nvim
+
+# Работа с Python
+# alias python3="python3.13"
+# alias python="python3.13"
+# alias actpy="source ./.venv/bin/activate" # для быстрой аквивации python
+
+# WSL Windos
 #alias open=explorer.exe # for windows wsl - open dir in ecploer windos: open .
