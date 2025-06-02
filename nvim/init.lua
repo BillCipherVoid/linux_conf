4f5343d0b9ab698f11fc78cda8d5605815dd59f8
BillCipherVoid <billciphervoid@gmail.com>
2025-06-03 00:13:05 +0300

    nvim add plug TODO
    Удобный плагин для работы с TODO
    Так же доавлен keymap: ,td для вызова поиска TODO

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,9 +8,9 @@ require("plugins.telescope") -- Поиск по файлам
 require("plugins.Coment")  -- Коментирование
 require("plugins.cmp")  -- Автокомлит
 require("plugins.lsp")  -- LSP
+require("plugins.todo-comments")  -- Работа с TODO
 require('auto-save').setup() -- Автосохранения 
 require("autoclose").setup() -- Автоматические двойные кавычки, скобки и тп. И работа с ними
-
 
 -- Color scheme
 vim.cmd([[colorscheme kanagawa-dragon]]) -- kanagawa-wave, kanagawa-dragon, kanagawa-lotus
