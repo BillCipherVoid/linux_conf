94260dd322a84332b948f44686428e781a7bba81
BillCipherVoid <billciphervoid@gmail.com>
2025-07-19 18:56:39 +0300

    zsh add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,8 +4,16 @@ alias n=nvim
 
 # Работа с Python
 alias python3="python3.13"
-alias python="python3.13"
+alias python="python3"
+alias py="python"
 alias actpy="source ./.venv/bin/activate" # для быстрой аквивации python
-
+alias mkpyproj="python3 -m venv .venv && 
+    cp ~/.config/other_files/pyrightconfig.json . && 
+    actpy && 
+    pip install -U pip &&
+    pip install pyright"
+alias mkdjproj="mkpyproj &&
+    pip install django django-stubs django-types django-extensions ipython &&
+    echo '--- Pls add djangon_extensions in INSTALLED_APPS'"
 # WSL Windos
 #alias open=explorer.exe # for windows wsl - open dir in ecploer windos: open .
