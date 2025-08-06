9f4b8225cbe2cb389fe9569f950929bb8027fc01
BillCipherVoid <billciphervoid@gmail.com>
2025-08-06 21:51:58 +0300

    rebase nvim in new repository

---
+++ bin/install_nvim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Клонируем репозиторий nvim_conf
+git clone https://github.com/Cipher-Void/nvim_conf ~/.config/nvim
+
+echo "\n\nКонфигурация Neovim установлена.  Перезапустите Neovim."
